bb5699d57f0a3ce5149a906dc10a8d502d27098f
CharissaHollister <charissa.hollister@diasorin.com>
2022-09-13 18:13:54 -0500

    add apollo to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,60 @@
 import "./App.css";
 import Nav from "./components/Nav";
-import PostCreate from './components/PostCreate'
-import Post from './components/Post'
-import Store from './components/Store'
+import PostCreate from "./components/PostCreate";
+import Post from "./components/Post";
+import Store from "./components/Store";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import NoMatch from "./pages/NoMatch";
+// import SingleThought from "./pages/SingleThought";
+// import Profile from "./pages/Profile";
+// import Signup from "./pages/Signup";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
-    <div>
-      <Nav id="nav-section" />
-      <main id="main-section">
-        <PostCreate />
-        <Post />
-        <Store />
-      </main>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Nav id="nav-section" />
+          <main id="main-section">
+            <PostCreate />
+            <Post />
+            <Store />
+          </main>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
