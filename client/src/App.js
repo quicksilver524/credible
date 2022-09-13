862cce63dc56c4953912691bf1d1b910ebd20edd
CharissaHollister <charissa.hollister@diasorin.com>
2022-09-13 18:31:17 -0500

    update home.js page to have post post create and store components instead of in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import Nav from "./components/Nav";
-import PostCreate from "./components/PostCreate";
-import Post from "./components/Post";
-import Store from "./components/Store";
+// import PostCreate from "./components/PostCreate";
+// import Post from "./components/Post";
+// import Store from "./components/Store";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
@@ -16,12 +16,12 @@ import { setContext } from "@apollo/client/link/context";
 // import Header from "./components/Header";
 // import Footer from "./components/Footer";
 
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 // import NoMatch from "./pages/NoMatch";
 // import SingleThought from "./pages/SingleThought";
 // import Profile from "./pages/Profile";
-// import Signup from "./pages/Signup";
+import Signup from "./pages/Signup";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -48,10 +48,17 @@ function App() {
         <div>
           <Nav id="nav-section" />
           <main id="main-section">
-            <PostCreate />
-            <Post />
-            <Store />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* <PostCreate />
+              <Post />
+              <Store /> */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
           </main>
+          <Footer id="footer-section" />
         </div>
       </Router>
     </ApolloProvider>
