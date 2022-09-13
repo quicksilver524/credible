6a3953a3f5d906743dd1aa31f6146a5ee89a3ecf
CharissaHollister <charissa.hollister@diasorin.com>
2022-09-13 18:36:32 -0500

    update app.js to only have primary references and to link to apollo/auth and added link to footer component to appear on all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 // import PostCreate from "./components/PostCreate";
 // import Post from "./components/Post";
 // import Store from "./components/Store";
+//! ^ moved to home page and only main references here
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
@@ -13,14 +15,11 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 // import NoMatch from "./pages/NoMatch";
-// import SingleThought from "./pages/SingleThought";
-// import Profile from "./pages/Profile";
+//todo ^ update if prefer a nomatch instead of signup like I made it 
 import Signup from "./pages/Signup";
 
 const httpLink = createHttpLink({
@@ -53,9 +52,9 @@ function App() {
               {/* <PostCreate />
               <Post />
               <Store /> */}
-              <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="*" element={<NoMatch />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Signup />} />
             </Routes>
           </main>
           <Footer id="footer-section" />
