3d0bc62fac40ca2817278b666b860d97837c242b
CharissaHollister <charissa.hollister@diasorin.com>
2022-09-13 08:22:19 -0500

    add server files similar to Module 21

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:3000/credible",
+  process.env.MONGODB_URI || "mongodb://localhost:3001/credible",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
