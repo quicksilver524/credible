198c4b0ef51208938b6fdc45469fd732b3d5361a
goforward-z <zlepeng.ucf@gmail.com>
2022-09-15 17:35:14 -0400

    default points can not be anegative number

--- server/models/User.js
+++ server/models/User.js
@@ -23,7 +23,8 @@ const userSchema = new Schema(
     points:{
         type: Number,
         required: true,
-        default: 10
+        default: 10,
+        min: 0
     },
     thoughts: [
       {
