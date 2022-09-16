a6251ec97082fa9496ee34df7cb9ea3bec2b02e2
CharissaHollister <charissa.hollister@diasorin.com>
2022-09-15 21:47:15 -0500

    completed login modal except for yes button functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,8 @@
 //teesting semantic ui modal
 
 import React from "react";
-import { Button, Form, Image, Modal } from "semantic-ui-react";
+import { Grid, Button, Form, Image, Modal } from "semantic-ui-react";
+import LoginImage from "../images/LoginImage1.jpg";
 
 function LoginModal() {
   const [open, setOpen] = React.useState(false);
@@ -15,26 +16,35 @@ function LoginModal() {
     >
       <Modal.Header>Login</Modal.Header>
       <Modal.Content image>
-        <Image size="medium" src="/images/avatar/large/rachel.png" wrapped />
-        {/* ^update to what we want */}
+        <Image size="medium" src={LoginImage} wrapped />
         <Modal.Description>
           <Form>
             <Form.Input fluid label="Email" type="email" placeholder="Email" />
-            <Form.Input label="Enter Password" type="password" />
+            <Form.Input
+              label="Password"
+              type="password"
+              placeholder="password"
+            />
           </Form>
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
-        <Button type="submit" color="black" onClick={() => setOpen(false)}>
-          Nope
-        </Button>
-        <Form.Button
-          content="Yep, that's me"
-          labelPosition="right"
-          icon="checkmark"
-          onClick={() => setOpen(false)}
-          positive
-        />
+        <Grid columns={2} relaxed="very">
+          <Grid.Column floated="left" width={4}>
+            <Button type="submit" color="black" onClick={() => setOpen(false)}>
+              I'm Not Credible
+            </Button>
+          </Grid.Column>
+          <Grid.Column floated="right" width={5}>
+            <Form.Button
+              content="Yep, that's me"
+              labelPosition="right"
+              icon="checkmark"
+              onClick={() => setOpen(false)}
+              positive
+            />
+          </Grid.Column>
+        </Grid>
       </Modal.Actions>
     </Modal>
   );
