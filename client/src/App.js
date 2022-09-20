af2351b69d5dbdb58e7a6d519588b84acf4e34bb
CharissaHollister <charissa.hollister@diasorin.com>
2022-09-19 19:41:59 -0500

    store card creation using semantic

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
-import "./App.css";
-
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   ApolloClient,
--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -4,64 +4,67 @@ import gold2 from "../pageImages/gold2.png";
 import gold3 from "../pageImages/gold3.png";
 import gold4 from "../pageImages/gold4.png";
 import { Container } from "semantic-ui-react";
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Card, Icon, Image, Button } from "semantic-ui-react";
 
 function Store() {
   return (
-    <Container>
-      <div id="store-section">
-        <div id="credit-section">
-          <div className="card-container store-container">
-            <div className="card credit-purchase-card">
-              <Card.Header>99 credits</Card.Header>
-              <Image src={gold1} alt="gold1" wrapped ui={false} />
-              <button
-                type="button"
-                className="credit-purchase-button post-create-btn"
-              >
-                Buy Credits
-              </button>
-            </div>
-          </div>
-          <div className="card-container store-container">
-            <div className="card credit-purchase-card">
-              <h3>249 credits</h3>
-              <img src={gold2} alt="gold2" />
-              <button
-                type="button"
-                className="credit-purchase-button post-create-btn"
-              >
-                Buy Credits
-              </button>
-            </div>
-          </div>
-          <div className="card-container store-container">
-            <div className="card credit-purchase-card">
-              <h3>499 credits</h3>
-              <img src={gold3} alt="gold3" />
-              <button
-                type="button"
-                className="credit-purchase-button post-create-btn"
-              >
-                Buy Credits
-              </button>
-            </div>
-          </div>
-          <div className="card-container store-container">
-            <div className="card credit-purchase-card">
-              <h3>999 credits</h3>
-              <img src={gold4} alt="gold4" />
-              <button
-                type="button"
-                className="credit-purchase-button post-create-btn"
-              >
-                Buy Credits
-              </button>
-            </div>
-          </div>
-        </div>
-        <div id="product-section"></div>
-      </div>
+    <Container id="store-section">
+      <Card.Group itemsPerRow={4} id="credit-section">
+        {/* <Container className="card-container store-container"> */}
+        <Card color="violet" className="card credit-purchase-card">
+          <Card.Header as="h2">99 credits</Card.Header>
+          <Image src={gold1} alt="gold1" />
+          <Card.Content>
+            <Icon name="hand point right" />
+            <Button
+              // background-color="#4baeae"
+              type="button"
+              className="credit-purchase-button post-create-btn"
+            >
+              Buy Credits
+            </Button>
+          </Card.Content>
+        </Card>
+        {/* </Container> */}
+        {/* <div className="card-container store-container"> */}
+        <Card color="violet" className="card credit-purchase-card">
+          <Card.Header as="h2">249 credits</Card.Header>
+          <Image src={gold2} alt="gold2" />
+          <Button
+            type="button"
+            className="credit-purchase-button post-create-btn"
+          >
+            <Icon name="hand point right" />
+            Buy Credits
+          </Button>
+        </Card>
+        {/* </div>
+        <div className="card-container store-container"> */}
+        <Card color="violet" className="card credit-purchase-card">
+          <Card.Header as="h2">499 credits</Card.Header>
+          <Image src={gold3} alt="gold3" />
+          <Button
+            type="button"
+            className="credit-purchase-button post-create-btn"
+          >
+            Buy Credits
+          </Button>
+        </Card>
+        {/* </div>
+        <div className="card-container store-container"> */}
+        <Card color="violet" className="card credit-purchase-card">
+          <Card.Header as="h2">999 credits</Card.Header>
+          <Image src={gold4} alt="gold4" />
+          <Button
+            type="button"
+            className="credit-purchase-button post-create-btn"
+          >
+            Buy Credits
+          </Button>
+        </Card>
+        {/* </div> */}
+      </Card.Group>
+      <div id="product-section"></div>
     </Container>
   );
 }
